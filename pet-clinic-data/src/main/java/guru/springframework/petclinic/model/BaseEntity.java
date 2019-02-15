287705bab47ae50f72285227a8264b14635d5b3c
sudhanshu <aryankedia@gmail.com>
2019-02-15 16:28:42 +0530

    lot of changes, in the owner controller, circle ci added for automated build.

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/BaseEntity.java
@@ -23,6 +23,7 @@ public class BaseEntity implements Serializable {
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
--- pet-clinic-web/src/main/java/guru/springframework/petclinic/Controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/Controller/OwnerController.java
@@ -6,13 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Set;
 
 @RequestMapping("/owners")
@@ -22,19 +21,55 @@ public class OwnerController {
     public static  final String  VIEWS_OWNER_CREATE_OR_UPDATE_FORM ="owners/createOrUpdateOwnerForm";
 
     @Autowired
-    private OwnerService ownerService;
+    private final OwnerService ownerService;
 
     public OwnerController(OwnerService ownerService){
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"","/","/find","/find.html"})
+    /*
+    @RequestMapping({"","/"})
     public String listOfOwner(Model model){
-
         Set<Owner> owners = ownerService.findAll();
         model.addAttribute("owners", owners);
         return "owners/find";
     }
+    */
+
+    /*
+    @InitBinder
+    public void setAllowedFields(WebDataBinder webDataBinder){
+        webDataBinder.setAllowedFields("id");
+    }
+    */
+    @RequestMapping("/find")
+    public String findOwners(Model model) {
+       Owner owner = new Owner();
+       model.addAttribute("owner", owner);
+       return "owners/findOwners";
+    }
+
+    @GetMapping
+    public String processFindForm(Owner owner, BindingResult result, Model model){
+       //allow parameterless GET request for /owner tor etunr all records
+        if (owner.getLastName() == null) {
+            owner.setLastName("");
+        }
+
+        List<Owner> results = ownerService.findAllByLastNameLike("%"+owner.getLastName() +"%");
+        if(results.isEmpty()){
+            result.rejectValue("lastName", "not found", "not found");
+            return "owners/findOwners";
+        }
+        else if(results.size() == 1){
+            owner = results.get(0);
+            return "redirect:/owners/" + owner.getId();
+        } else {
+            model.addAttribute("selections", results);
+            return "owners/ownersList";
+        }
+    }
+
     @GetMapping("/{ownerId}")
         public ModelAndView showOwner(@PathVariable  Long ownerId){
         ModelAndView mav = new ModelAndView("owners/ownerDetails");
@@ -56,12 +91,9 @@ public class OwnerController {
         else{
             owner.setId(ownerId);
             Owner savedOwner = ownerService.save(owner);
-            return "redirect/owners/" +savedOwner.getId();
+            return "redirect:/owners/" +savedOwner.getId();
         }
-
     }
-
-
 
     @GetMapping("/new")
     public String initCreationForm(Model model){
@@ -75,13 +107,12 @@ public class OwnerController {
         if(result.hasErrors())  {
             return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
         }
+
         else {
-            ownerService.save(owner);
-            return "redirect:/owners/"  +owner.getId();
+            Owner savedOwner = ownerService.save(owner);
+            return "redirect:/owners/"  +savedOwner.getId();
         }
     }
-
-
 }
 
 
